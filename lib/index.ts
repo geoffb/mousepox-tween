90a4ffac27e85341358b8dd75390831d9662aba1
Geoff Blair <geoff@mousepox.io>
2024-12-29 19:00:56 -0700

    Add Back easing

--- lib/index.ts
+++ lib/index.ts
@@ -20,4 +20,24 @@ export const Ease = {
     }
   },
 
+  BackIn(k: number): number {
+    const c1 = 1.70158;
+    const c3 = c1 + 1;
+    return c3 * k * k * k - c1 * k * k;
+  },
+
+  BackOut(k: number): number {
+    const c1 = 1.70158;
+    const c3 = c1 + 1;
+    return 1 + c3 * Math.pow(k - 1, 3) + c1 * Math.pow(k - 1, 2);
+  },
+
+  BackInOut(k: number): number {
+    const c1 = 1.70158;
+    const c2 = c1 * 1.525;
+    return k < 0.5
+      ? (Math.pow(2 * k, 2) * ((c2 + 1) * 2 * k - c2)) / 2
+      : (Math.pow(2 * k - 2, 2) * ((c2 + 1) * (k * 2 - 2) + c2) + 2) / 2;
+  }
+
 };
